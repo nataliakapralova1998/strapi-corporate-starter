ee730b0ddfd21aa7a1c20c559126914c316a6c2c
nataliakapralova1998 <n.kapralovaa@gmail.com>
2025-01-23 17:31:56 +0100

    updates on db

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,11 +1,15 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = ({ env }) => ({
   connection: {
-    client: 'sqlite',
+    client: 'postgres',
     connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      host: env('DATABASE_HOST', 'localhost'),
+      port: env('DATABASE_PORT', 5432),
+      database: env('DATABASE_NAME', 'strapi_database'),
+      user: env('DATABASE_USERNAME', 'nataliakapralova'),
+      password: env('DATABASE_PASSWORD', 'password'),
+      ssl: false, // Make sure SSL is disabled if not using it
     },
-    useNullAsDefault: true,
   },
 });
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,22 +3,22 @@ module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      // colors: {
-      //   background: "#faf8f4",
-      //   primary:"rgb(139 92 246)",
-      //   secondary: "rgb(236 72 153)",
-      //   text: {
-      //     DEFAULT: "#1e1e14",
-      //   },
-      // },
       colors: {
-        background: "#fdf6e3", // A soft beige for a warm, inviting background
-        primary: "#2a9d8f", // A deep teal for primary elements
-        secondary: "#e76f51", // A muted coral for accents
+        background: "#faf8f4",
+        primary:"rgb(139 92 246)",
+        secondary: "rgb(236 72 153)",
         text: {
-          DEFAULT: "#264653", // A dark slate for strong contrast and readability
+          DEFAULT: "#1e1e14",
         },
       },
+      // colors: {
+      //   background: "#fdf6e3", // A soft beige for a warm, inviting background
+      //   primary: "#2a9d8f", // A deep teal for primary elements
+      //   secondary: "#e76f51", // A muted coral for accents
+      //   text: {
+      //     DEFAULT: "#264653", // A dark slate for strong contrast and readability
+      //   },
+      // },
       animation: {
         "pulse-vertical": "pulse-vertical 2s ease-in-out infinite",
       },
