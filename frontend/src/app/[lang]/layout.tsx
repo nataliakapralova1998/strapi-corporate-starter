19a1539431afaea60a9c4bd8af29f00a7c90ee31
nataliakapralova1998 <n.kapralovaa@gmail.com>
2025-04-28 14:23:15 +0200

    attempt to fix build

--- frontend/src/app/[lang]/layout.tsx
+++ frontend/src/app/[lang]/layout.tsx
@@ -9,6 +9,8 @@ import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 import { FALLBACK_SEO } from "@/app/[lang]/utils/constants";
 
+export const dynamic = 'force-dynamic'
+
 async function getGlobal(lang: string): Promise<any> {
   const token = process.env.NEXT_PUBLIC_STRAPI_API_TOKEN;
 
